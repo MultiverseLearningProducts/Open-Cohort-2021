96aca40e827e3b336d842e8e62f824f40929f652
CoachTonyG <tony.graham@multiverse.io>
2021-09-15 13:07:52 -0400

    tony test

---
+++ CountBits.js
@@ -0,0 +1,8 @@
+var countBits = function(n) {
+    // Program me
+    const result = n.toString(2)
+    const binary = result.map()
+    return binary
+  };
+  const number = prompt(':');
+  countBits(number);
