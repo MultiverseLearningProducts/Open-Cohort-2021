fb4034d0e65abb614e5a15fac50943bfd1ea624a
= <90lineng@gmail.com>
2021-09-29 09:30:42 -0400

    static scaffolds updated

---
+++ code-samples/Bootcamp/week_1/Static_Functions_And_Properties/pokemon_scaffold.js
@@ -0,0 +1,29 @@
+class Pokemon {
+    //reveal this last
+    static pokemons = []
+  
+    constructor(name, number, type, hp){
+    }
+  
+    static nameIsString(name) {
+    }
+  
+    static numberIsTypeNumber(number) {
+    }
+  
+    static isValidType(type) {
+      const elements = [] //what can we put inside of this array?
+      //What kind of array method can we use to check if the type is an element?
+    }
+  
+    static isValidHp(hp) {
+    }
+  
+  
+    //what kind of argument should be passed into this method?
+    assignType() {
+  
+    }
+  
+  }
+  
