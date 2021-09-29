f99a821b6fdcfbc70edbbada343989f44078ade4
= <90lineng@gmail.com>
2021-09-29 14:23:00 -0400

    pokemon scaffold solution added

--- code-samples/Bootcamp/week_1/Static_Functions_And_Properties/pokemon_scaffold.js
+++ code-samples/Bootcamp/week_1/Static_Functions_And_Properties/pokemon_scaffold.js
@@ -3,27 +3,43 @@ class Pokemon {
     static pokemons = []
   
     constructor(name, number, type, hp){
+      this.name = name
+      this.number = number
+      this.type = type
+      this.hp = hp
     }
   
     static nameIsString(name) {
+      return typeof name === 'string'
     }
   
     static numberIsTypeNumber(number) {
+      return typeof number === 'number'
     }
   
     static isValidType(type) {
-      const elements = [] //what can we put inside of this array?
+      const elements = ['fire', 'fairy', 'grass', 'earth', 'lighting', 'ghost', 'poison'] //what can we put inside of this array?
       //What kind of array method can we use to check if the type is an element?
+      return (elements.includes(type)) 
+
     }
   
     static isValidHp(hp) {
+      // 50hp - 250hp
+      (hp >= 50 && hp <= 250) ? true : false
     }
   
   
     //what kind of argument should be passed into this method?
-    assignType() {
-  
+    assignType(type) {
+      this.type = type
+    }
+    
+    checkName(name) {
+      return typeof this.name === 'string'
     }
   
   }
-  
+
+
+module.exports = Pokemon
