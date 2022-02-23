6d143f8e2784c24b7dc0b18e9f3f0405b5265db0
= <90lineng@gmail.com>
2022-02-23 09:10:35 -0500

    accessibility demo added

---
+++ code-samples/Mod_1/code-samples/accessibility/plants-direct-solution/src/App.js
@@ -0,0 +1,40 @@
+import data from './products.json'
+import { useState } from 'react'
+import { Product } from './Product'
+import './App.css';
+
+function App() {
+  const [cart, setCart] = useState(data.cart)
+  const [cartStatus, setStatus] = useState({})
+
+
+  const addToCart = (product) => {
+      setCart([...cart, product])
+  }
+
+  const removeFromCart = (product) => {
+    setCart(cart.filter(item => item !== product))
+  }
+
+  const setCartStatus = (key) => {
+    setStatus({...cartStatus, [key]: !cartStatus[key]})
+    console.log('THIS IS CART STATUS', cartStatus)
+  }
+
+  return (
+    <div className="App">
+      {data.products.map((product) => 
+        <Product key={product.productId} id={product.productId} product={product} addToCart={addToCart} 
+        removeFromCart={removeFromCart} setCartStatus={setCartStatus} cartStatus={cartStatus[product.productId]} />
+      )
+      }
+      <p>Items in cart {cart.length}</p>
+    </div>
+  );
+}
+
+export default App;
+
+//we need to give each child component access to the image file ?? [x]
+// give each child component access to features.[x]
+// link stylesheet [x]
